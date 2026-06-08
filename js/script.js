20f21f1a13ded1512be50d08c1aba4d1cb0a725c
JAY SOUZA DIAS <124062266+j4yh4ck3r@users.noreply.github.com>
2026-06-08 00:34:38 -0300

    Refactor shopping cart functions for clarity

--- js/script.js
+++ js/script.js
@@ -70,7 +70,7 @@ function enviarPedidoWhatsApp() {
 
         mensagem += `\nEndereço da entrega: ${endereco}`;
 
-        let numero = "5532991150384"; // exemplo
+        let numero = "553299999-9999"; // exemplo
         let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
         window.open(url, "_blank");
     }
